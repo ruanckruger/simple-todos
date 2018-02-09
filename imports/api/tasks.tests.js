b7593990d0205c4f347d817d460378b52ccc3387
ruanckruger <ruanckruger@gmail.com>
2018-02-09 08:45:23 +0200

    first commit

---
+++ imports/api/tasks.tests.js
@@ -0,0 +1,30 @@
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { assert } from 'chai';
+import { Tasks } from './tasks.js';
+
+if (Meteor.isServer){
+    describe('Tasks',() => {
+        describe('methods',() => {
+            const userId = Random.id();
+            let taskid;
+
+            beforeEach(() => {
+                Tasks.remove({});
+                taskid = Tasks.insert({
+                    text: 'test task',
+                    createdAt: new Date(),
+                    owner: userId,
+                    username: 'tmeasday'
+                });
+            });
+            it('can delete owned task', () => {
+                const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+                const invocation = { userId };
+
+                deleteTask.apply(invocation, [taskId]);
+                assert.equal(Tasks.find().count(), 0);
+            });
+        });
+    });
+}
